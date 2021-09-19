833b61a8b6b89d22fad78764f710bb77192b5165
Raghul Krishnan <raghul.sathya@gmail.com>
2021-09-18 23:40:19 -0400

    Routing added and bare screens created

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Welcome from './Welcome';
+import SwipedScreen from './SwipedScreen';
+import Reasons from './reasons';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <StatusBar style="auto" />
+        <Stack.Navigator initialRouteName="welcome" screenOptions={{headerShown: false}}>
+          <Stack.Screen name="welcome" component={Welcome} options={{animation: "none"}}></Stack.Screen>
+          <Stack.Screen name="swiped_screen" component={SwipedScreen} options={{animation: "none"}}></Stack.Screen>
+          <Stack.Screen name="reasons" component={Reasons} options={{animation: "none"}}></Stack.Screen>
+        </Stack.Navigator> 
+    </NavigationContainer>
   );
 }
 
