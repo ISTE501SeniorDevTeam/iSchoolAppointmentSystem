a345d9ab7c487a258571b0b506a4078571c94726
FredAmartey <fdamartey@gmail.com>
2021-10-25 21:38:14 -0400

    changes for selecting student

--- frontend/advisor_frontdesk/src/advisor/components/Student/student.component.jsx
+++ frontend/advisor_frontdesk/src/advisor/components/Student/student.component.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./student.styles.css"
 
-let selectedStudent = () =>{
-    
-}
 
-export const Student = (props) => (
-    <button onClick={selectedStudent} className="StudentNameButtonContainer">
+//   const SelectedStudent = () => {
+//     const [isActive, setActive] = useState("false");
+//     setActive(!isActive); 
+//    };
+
+export const Student = (props) => {
+    const [isActive, setActive] = useState("false");
+
+    const SelectedStudent = () => {
+        setActive(!isActive); 
+    };
+    
+    return(
+    <button onClick={SelectedStudent} 
+    className={"StudentNameButtonContainer"}>
             <span className="StudentName">{props.student.studentName}</span>
             {/* {console.log(props.student.studentName)} */}
     </button>
+    )
 
-
-)
+}
--- frontend/advisor_frontdesk/src/advisor/components/StudentList/studentlist.component.jsx
+++ frontend/advisor_frontdesk/src/advisor/components/StudentList/studentlist.component.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import { Student } from "../Student/student.component";
 import "./studentlist.styles.css"
 
-export const StudentList = (props) => (
+export const StudentList = (props) => {
+    const [isActive, setActive] = useState("false");
+
+    const SelectedStudent = () => {
+        setActive(!isActive); 
+    };
+
+    // <div key={student.studentUID} className={`StudentNameContainer ${student.isActive ? "active" : ""}` }></div>
+
+    // <span className="StudentName">{student.studentName}{student.isActive}</span>
+
+    return(
     <div className="StudentContainer">
         {props.students.map(student => (
-            <Student key={student.studentUID} student={student}></Student>
+            <Student onClick={SelectedStudent} key={student.studentUID} student={student} className={`StudentNameContainer ${student.isActive ? "active" : ""}`}>{student.isActive}
+            {console.log(student.isActive)}
+            </Student>
+            
         ))}
     </div>
-)
+)}
